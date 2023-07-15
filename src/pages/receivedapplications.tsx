b6533d2df2e4b8bb2ad2f142b6cc5d14e333531b
Vairamuthu <mvairamuthu2003@gmail.com>
2023-07-16 01:41:51 +0530

    updated the dashboard

--- src/pages/receivedapplications.tsx
+++ src/pages/receivedapplications.tsx
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic';
 import { GETLOAN } from './BlockchainServices';
 import { useAccount } from 'wagmi';
 import { format } from 'date-fns';
+import { PieChart,Tooltip, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
 
 const Header = dynamic(() => import('./components/Header'), {
   ssr: false,
@@ -36,41 +37,104 @@ const Receivedapplications = () => {
     return year;
   }
 
+ // Calculate the counts for approved and unapproved applications
+const approvedCount = field.filter((data) => data[4] === true).length;
+const unapprovedCount = field.filter((data) => data[4] === false).length;
+
+// Calculate the total loan amount and average loan amount
+const totalLoanAmount = field.reduce((total, data) => total + parseInt(data[2]), 0);
+const averageLoanAmount = totalLoanAmount / field.length;
+
+// Calculate the number of applications that were paid and not paid
+const paidCount = field.filter((data) => data[8] === true).length;
+const notPaidCount = field.filter((data) => data[8] === false).length;
+  // Pie chart data
+
+
+  // Calculate the number of sanctioned and not sanctioned loans
+  const sanctionedCount = field.filter((data) => data[6] === true).length;
+  const notSanctionedCount = field.filter((data) => data[6] === false).length;
+
+  // Pie chart data
+  const pieChartData = [
+    { name: 'Approved', value: approvedCount, label: 'Approved' },
+    { name: 'Unapproved', value: unapprovedCount, label: 'Unapproved' },
+    { name: 'Paid', value: paidCount, label: 'Paid' },
+    { name: 'Not Paid', value: notPaidCount, label: 'Not Paid' },
+    { name: 'Sanctioned', value: sanctionedCount, label: 'Sanctioned' },
+    { name: 'Not Sanctioned', value: notSanctionedCount, label: 'Not Sanctioned' },
+  ];
+
+  // Custom colors for the pie chart
+  const colors = ['#00C853', '#F44336', '#2196F3', '#FF9800', '#FFEB3B', '#9E9E9E'];
+
+
   return (
     <div className='text-center bg-[#140506]'>
       <Header />
       <h1 className='text-center text-3xl mt-20 font-bold py-10 bg-[#140506] text-green-700'>
         Received Application
       </h1>
-      <div className='px-5 md:mx-20 overflow-x-scroll md:overflow-hidden'>
-  <table className='md:ml-20'>
-    <tr className='space-x-5 border-white border-2 text-2xl text-green-300'>
-      <th className='p-5'>Farmer Name</th>
-      <th className='p-5'>Aadhaar Number</th>
-      <th className='p-5'>Amount of loan</th>
-      <th className='p-5'>Reason for Loan</th>
-      <th className='p-5'>Status</th>
-      <th className='p-5'>Applied At</th>
-       <th className='p-5'>Loan Sanctioned</th>
-      <th className='p-5'>Deadline</th>
-      <th className='p-5'>IsPaid</th>
-    </tr>
-    {field?.map((data: any, index) => (
-      <tr className='border-white text-lg border-2  text-white text-center' key={index}>
-        <td>{data[0]}</td>
-        <td>{parseInt(data[1])}</td>
-        <td>{parseInt(data[2])}</td>
-        <td>{data[3]}</td>
-        <td>{data[4] === true ? 'approved' : 'unapproved'}</td>
-        <td>2023</td>
-        <td>{data[6] === true ? "Sanctioned" : "Not Sanctioned"}</td>
-      <td>30</td>
-      <td>{data[8] === true ? "Paid" : "Not Paid"}</td>
-      </tr>
-    ))}
-  </table>
-</div>
+      <div className=''>
+        {/* Render the pie chart */}
+        <ResponsiveContainer width='100%' height={420}>
+          <PieChart>
+            <Pie
+              data={pieChartData}
+              dataKey='value'
+              nameKey='label'
+              cx='50%'
+              cy='50%'
+              outerRadius={180}
+              fill='#8884d8'
+              label={(entry) => `${entry.label} (${(entry.value * 100 / totalLoanAmount).toFixed(2)}%)`}
+            >
+              {pieChartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend align='center' verticalAlign='bottom' />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
 
+      <div className='px-5 md:mx-20'>
+        <div className='text-white pb-5 text-xl'>
+        <p>Total Loan Amount: {totalLoanAmount}</p>
+          <p>Average Loan Amount: {averageLoanAmount}</p>
+          <p>Sanctioned Applications: {sanctionedCount}</p>
+          <p>Not Sanctioned Applications: {notSanctionedCount}</p>
+        </div>
+      </div>
+      <div className='px-5 md:mx-20 pb-5 overflow-x-scroll md:overflow-hidden'>
+        <table className='md:ml-20 '>
+          <tr className='space-x-5 border-white border-2 text-2xl text-green-300'>
+            <th className='p-5'>Farmer Name</th>
+            <th className='p-5'>Aadhaar Number</th>
+            <th className='p-5'>Amount of loan</th>
+            <th className='p-5'>Reason for Loan</th>
+            <th className='p-5'>Status</th>
+            <th className='p-5'>Applied At</th>
+            <th className='p-5'>Loan Sanctioned</th>
+            <th className='p-5'>Deadline</th>
+            <th className='p-5'>IsPaid</th>
+          </tr>
+          {field?.map((data: any, index) => (
+            <tr className='border-white text-lg border-2  text-white text-center' key={index}>
+              <td>{data[0]}</td>
+              <td>{parseInt(data[1])}</td>
+              <td>{parseInt(data[2])}</td>
+              <td>{data[3]}</td>
+              <td>{data[4] === true ? 'approved' : 'unapproved'}</td>
+              <td>2023</td>
+              <td>{data[6] === true ? 'Sanctioned' : 'Not Sanctioned'}</td>
+              <td>30</td>
+              <td>{data[8] === true ? 'Paid' : 'Not Paid'}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 };
